252a500a39227e54cfd79c31b8548936650f8642
Michael Graf <michael.graf3110@gmail.com>
2022-09-01 22:33:46 +0200

    key generation with rust lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 use neon::prelude::*;
+use secure_survey::*;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
+fn generate_key_pair(mut cx: FunctionContext) -> JsResult<JsString> {
+    let key_pair = KeyPair::new();
+    Ok(cx.string(serde_json::to_string(&key_pair).unwrap()))
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
+    cx.export_function("generate_key_pair", generate_key_pair)?;
     Ok(())
 }
